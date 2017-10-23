b5b247328fe7c8d5b02e70ba521c2763c20dae9b
Sivhaun <sivhaun@gmail.com>
2017-10-23 17:33:16 -0400

    AtomicGamesAI

---
+++ src/com/atomicobject/rts/test.java
@@ -0,0 +1,11 @@
+package com.atomicobject.rts;
+
+public class test {
+
+    public static void main(String[] args) {
+        Location loc = new Location(6,5);
+        System.out.println(loc);
+    }
+
+
+}
